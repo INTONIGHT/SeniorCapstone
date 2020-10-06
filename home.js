14387fa556c017f719b47ddc30064705881c47fc
INTONIGHT <71537873+INTONIGHT@users.noreply.github.com>
2020-10-05 22:15:17 -0400

    Tried modifying the parsing of the JSON to little affect will try more tomorrow

--- home.js
+++ home.js
@@ -1,26 +1,18 @@
 //The store json file contains the store info
 //specificlaly the times required and employees required for equations.
-function readFile(file,callback){
-    let rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            callback(rawFile.responseText);
-        }
-    }
-    rawFile.send(null);
-}
-//readFile("C:\Users\tyler\OneDrive\Desktop\SeniorCapstone\employees.json",function(text){
-   // let data= JSON.parse(text);
-   // console.log(data);
-//})
-function createSpreadsheet(){
-    readFile("C:\Users\tyler\OneDrive\Desktop\SeniorCapstone\employees.json",function(text){
-        let data= JSON.parse(text);
-        console.log(data);
-    })
 
+
+function createSpreadsheet(){
+  let request = new XMLHttpRequest();
+  request.open("GET","store.json",false);
+  request.send(null);
+  request.onreadystatechange = function(){
+      if(request.readyState === 4 && request.status ===20){
+        let my_JSON_object = JSON.parse(request.responseText);
+        console.log(my_JSON_object);
+      }
+  }
+  
 }
 
 function adjustSpreadsheet(){
