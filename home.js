70880ca114d35aab73a270cdedb01fb1bbf6fcb1
INTONIGHT <71537873+INTONIGHT@users.noreply.github.com>
2020-10-08 16:15:16 -0400

    Got the error message to go wayby removing the null part still no display

--- home.js
+++ home.js
@@ -4,15 +4,15 @@
 
 function createSpreadsheet(){
   let request = new XMLHttpRequest();
-  request.open("GET","store.json",false);
-  request.send(null);
+  request.open("GET","store.json",true);
+  
   request.onreadystatechange = function(){
       if(request.readyState === 4 && request.status ===20){
+        
         let my_JSON_object = JSON.parse(request.responseText);
         console.log(my_JSON_object);
       }
   }
-  
 }
 
 function adjustSpreadsheet(){
