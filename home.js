313cbf6e0101b1ef6f8bea7c88a4fd07622d5825
INTONIGHT <71537873+INTONIGHT@users.noreply.github.com>
2020-11-11 13:50:53 -0500

    Adding a chart functionality to display the data currently gives a 404 error will figure out later

--- home.js
+++ home.js
@@ -15,6 +15,32 @@ function createSpreadsheet() {
   }
   // Send the request
   request.send();
+  //attempting to create a chart here
+  JSC.Chart("chartDiv", {
+    series: [
+      {
+        points: [{ x: "A", y: 10 }, { x: "B", y: 5 }]
+      }
+    ]
+  });
+ 
+  //hopefully it gets the data from the store.json
+  JSC.fetch("store.json")
+  .then(response => response.text())
+  .then(text => {
+    //Use csv text
+  });
+  //this uses dates but will modify
+  let points = data.map(d => {
+    return { x: d.date, y: d.actual };
+  });
+  //this renders the chart
+  const chart = new JSC.Chart("chartDiv", {
+    // Pass points to the series
+    series: [{ points: points }],
+    // Set the x axis scale to time.
+    xAxis_scale_type: "time"
+  });
 }
 
 function adjustSpreadsheet(){
