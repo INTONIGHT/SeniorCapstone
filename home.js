028461726df699f945d2e326e7fc01491150023f
Eric Saupe <ericsaupe@gmail.com>
2020-11-10 20:27:20 -0800

    Fixes button for fetching data
    The XMLHttpRequest was never actually being sent. Adding
    request.send() to the end of the function makes the actual request
    happen. The second bug was status === 20. HTTP statuses start at
    100 and go up to 500. Anything in the 200 range is a successful
    response with 200 being the default success response status.
    
    The code written at this point prints the JSON object to the console.
    
    References:
    https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/send
    https://developer.mozilla.org/en-US/docs/Web/HTTP/Status

--- home.js
+++ home.js
@@ -1,18 +1,19 @@
-//The store json file contains the store info
-//specificlaly the times required and employees required for equations.
+// The store json file contains the store info
+// specifically the times required and employees required for equations.
 
-
-function createSpreadsheet(){
+function createSpreadsheet() {
   let request = new XMLHttpRequest();
   request.open("GET","store.json",true);
-  
-  request.onreadystatechange = function(){
-     if(request.readyState === 4 && request.status === 20){
-        
-        let my_JSON_object = JSON.parse(request.responseText);
-        console.log(my_JSON_object);
-      }
+
+  request.onreadystatechange = function() {
+    // readyState 4 is "Done"
+    // status is 200 for "OK"
+    if (request.readyState === 4 && request.status === 200) {
+      let my_JSON_object = JSON.parse(request.responseText);
+      console.log(my_JSON_object);
+    }
   }
+  // Send the request
   request.send();
 }
 
